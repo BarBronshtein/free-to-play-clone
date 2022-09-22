151e9e7ce6722590f905a60dc37738a897d97bae
BarBronshtein <barbronshtein@gmail.com>
2022-09-22 10:11:17 +0300

    first commit

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex';
+const store = createStore({
+	strict: true,
+	state: {},
+	getters: {},
+	mutations: {},
+	actions: {},
+	modules: {},
+});
+export default store;
